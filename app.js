f53e70e5aeef2e3824dcd81f0805fba932f0e950
Peyman Mortazavi <pey.mortazavi@gmail.com>
2015-12-09 09:28:49 -0700

    fixed the id bug

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ io.on('connection', function(socket) {
       var policiesDict = {}
 
       for(var i = 0; i < policies.length; i++) {
-        policiesDict[policies[i].id] = 0
+        policiesDict[policies[i]._id] = 0
       }
 
       for(var i = 0; i < candidates.length; i++) {
-        candidatesDict[candidates[i]] = 0
+        candidatesDict[candidates[i]._id] = 0
       }
 
       for(var i = 0; i < docs.length; i++) {
